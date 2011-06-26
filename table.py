365560853147455d54b61fab312b149affbb0f4a
Ismaeil Abouljamal <izus@514568.no-reply.drupal.org>
2011-06-27 01:35:21 +0200

    ordonnacement d'une list

--- table.py
+++ table.py
@@ -3,10 +3,24 @@
 
 class Table(object):
   
-  """ Une Table 0/1
+  """ Une Table 0/1 ser aconsidérée comme une liste de listes (colonnes).
+  Exemple : 
+  1 1 1 0 0
+  1 0 0 0 1
+  0 0 1 1 0
+  est :
+  [[1, 1, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1], [0, 1, 0]]
   """
   def __init__(self, liste):
     self.donnee = liste
   
   def affiche(self):
     return self.donnee
+
+  def reorderListe(self):
+    li = self.donnee
+    li.sort()
+    return li
+    
+  def reorderTab(self):
+    pass
--- test_table.py
+++ test_table.py
@@ -22,11 +22,21 @@ class TableAffichageTest(unittest.TestCase):
   Classe de tests du réordonnancement de la table 0/1.
   """
 
-  def test_affiche1(self):
-    table1 = Table([1, 0])
-    self.assertEquals(table1.affiche(),[1,0])
-    #~ self.assertEquals(reorder([1,0]),[0,1])
-    #~ self.assertRaises(ZeroDivisionError, division, 10, 0)    
+  def test_affiche(self):
+    #~ t = Table([1, 0, 1, 0])
+    t1 = Table([[1, 1, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1], [0, 1, 0]])
+    #~ self.assertEquals(t.affiche(),[1, 0, 1, 0])
+    self.assertEquals(t1.affiche(),[[1, 1, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1], [0, 1, 0]])
+  
+  def test_reorderListe(self):
+    l = Table([1, 1, 0, 1, 0])
+    self.assertEquals(l.reorderListe(),[0, 0, 1, 1, 1])
+
+  
+  def test_reorderTab(self):
+    #~ t = liste de listes
+    #~ self.assertEquals(t.reorderTab(),liste de listes ordonnées)
+    pass
 
 if __name__ == '__main__':
   unittest.main()
