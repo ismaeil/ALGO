43f8a8071404527868866b4041f2634fe5f28472
Ismaeil Abouljamal <izus@514568.no-reply.drupal.org>
2011-06-27 01:45:27 +0200

    correction du texte de commentaire

--- table.py
+++ table.py
@@ -3,13 +3,13 @@
 
 class Table(object):
   
-  """ Une Table 0/1 ser aconsidérée comme une liste de listes (colonnes).
+  """ Une Table 0/1 sera considérée comme une liste de listes (lignes).
   Exemple : 
   1 1 1 0 0
   1 0 0 0 1
   0 0 1 1 0
   est :
-  [[1, 1, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1], [0, 1, 0]]
+  [[1, 1, 1, 0, 0], [1, 0, 0, 0, 1], [0, 0, 1, 1, 0]]
   """
   def __init__(self, liste):
     self.donnee = liste
