5c74e9b617a57a6c072e174ae3f11037a71f6a33
Ismaeil Abouljamal <isma404@yahoo.com>
2011-06-29 18:37:49 +0200

    Enlever toutes les méthodes non utilisées et leurs tests

--- table.py
+++ table.py
@@ -14,51 +14,7 @@ class Table(object):
   
   def __init__(self, liste):
     self.donnee = liste
-  
-  def affiche(self):
-    """"
-    Afficher une table en mode liste de listes (colonnes)
-    """
-    return self.donnee
-  
-  def reorderListe(self):
-    """
-    réordonner une liste (vue comme table) en mettant les 0 à gauche et les 1 à droite
-    """
-    li = self.donnee
-    li.sort()
-    return li
-  
-  def permCol(self, i, j):
-    """
-    échange les colonnes i et j
-    """
-    self.donnee[i], self.donnee[j] = self.donnee[j], self.donnee[i]
-    return self.donnee
-  
-  def keyZeroUn(self):
-    """
-    à priori cette méthode est indépendante de la classe Table
-    entrée = une liste de 0 1 par exemple [1, 0, 0, 0, 1]
-    sortie = une liste de deux liste la première contient les clés des 0 et la seconde
-    les clés de 1 donc : [[1,2,3], [0,4]]
-    """
-    li = self.donnee
-    un =[]
-    zero =[]
-    for i in range(len(li)):
-      if li[i] == 1:
-        un += [i]
-      else:
-        zero += [i]
-    return [zero, un]
-  
-  def getLigne(self, ligne):
-    li = []
-    for i in range(len(self.donnee)):
-      li.append(self.donnee[i][ligne])
-    return li
-  
+
   def nouvelOrdre(self, liste):
     """
     réordonner les colonnes suivant la liste en tant qu'index
--- test_table.py
+++ test_table.py
@@ -21,34 +21,6 @@ class TableAffichageTest(unittest.TestCase):
   Classe de tests du réordonnancement de la table 0/1.
   """
 
-  def test_affiche(self):
-    #~ t = Table([1, 0, 1, 0])
-    t1 = Table([[1, 1, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1], [0, 1, 0]])
-    #~ self.assertEquals(t.affiche(),[1, 0, 1, 0])
-    self.assertEquals(t1.affiche(),[[1, 1, 0], [1, 0, 0], [1, 0, 1], [0, 0, 1], [0, 1, 0]])
-  
-  def test_reorderListe(self):
-    """cette méthode ne sert à rien puisque dans une vrai table on 
-    échangera les colonnes
-    """
-    l = Table([1, 1, 0, 1, 0])
-    self.assertEquals(l.reorderListe(),[0, 0, 1, 1, 1])
-  
-  def test_permCol(self):
-    t = Table([[0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 0]])
-    self.assertEquals(t.permCol(0, 2),[[1, 0, 0], [1, 1, 1], [0, 0, 1], [1, 1, 0]])
-  
-  def test_keyZeroUn(self):
-    t = Table([1, 0, 0, 0, 1])
-    self.assertEquals(t.keyZeroUn(),[[1, 2, 3], [0, 4]])
-    t2 = Table([1, 0, 0, 0, 1, 1, 1, 1])
-    self.assertEquals(t2.keyZeroUn(),[[1, 2, 3], [0, 4, 5, 6, 7]])
-  
-  def test_getLigne(self):
-    t = Table([[0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 0]])
-    self.assertEquals(t.getLigne(2),[1, 1, 0, 0])
-    self.assertEquals(t.getLigne(1),[0, 1, 0, 1])
-
   def test_nouvelOrdre(self):
     """
     ordonne les colonnes dans l'ordre passé en argument à l'aide d'une liste
@@ -68,24 +40,23 @@ class TableAffichageTest(unittest.TestCase):
     t = goodListe([set([]), set([3]), set([]), set([4]), set([1]), set([]), set([]), set([2])])
     self.assertEquals(t, [2, 3, 0, 1] )
   
-  def test_reorderTab(self):
+  def test_reorderTab1(self):
     """
     0 1 1 1                           1 1 0 1
     0 1 0 1     se transforme en      0 1 0 1
     1 1 0 0                           0 0 1 1
     """
-    #~ t = Table([[0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 0]])
-    #~ self.assertEquals(t.reorderTab(),[3, 4, 1, 2])
     t = Table([[0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 0]])
     self.assertEquals(t.reorderTab(),[[1, 0, 0], [1, 1, 0], [0, 0, 1], [1, 1, 1]])
+
+  def test_reorderTab2(self):
+    """
+    1 0 0 0                           0 0 0 1
+    1 1 0 1     se transforme en      1 0 1 1
+    1 1 1 0                           0 1 1 1
+    """
     t2 = Table([[1, 1, 1], [0, 1, 1], [0, 0, 1], [0, 1, 0]])
     self.assertEquals(t2.reorderTab(),[[0, 1, 0], [0, 0, 1], [0, 1, 1], [1, 1, 1]])
-    
-  def test_isOrderUnique(self):
-    """"
-    l'idée est de voir si une permutation de colonne donnerait un ordre final totalement différent
-    """
-    pass
 
 if __name__ == '__main__':
   unittest.main()
