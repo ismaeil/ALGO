5c502f985ca96692e12ec84ed1d28f5bf704653a
Ismaeil Abouljamal <isma404@yahoo.com>
2011-07-11 23:01:30 +0200

    save before delating invLigne methode

--- table.py
+++ table.py
@@ -243,53 +243,50 @@ def maj(pos_c, pos_l, L, C, c0, c1, l0, l1):
     new_pos_l = pos_l
   
   return new_pos_c, new_pos_l, L, C
-  
-def invLigne(T):
-  for ligne in T:
-    ligne.reverse()
-  return T
 
-#~ def algo(T):
-  #~ """
-  #~ à partir d'une table, on rend la liste des lignes et des colonnes après application de l'algo
-  #~ Si on veux, on peut appliquer la méthode nouvelOrdreLC pour réordonner ainsi la table.
-  #~ Exemple :
-  #~ à partir de :
-      #~ c0 c1 c2 c3 c4 c5
-  #~ l0* 1  0  0  1  0  1
-  #~ l1* 1  1  1  0  1  1
-  #~ l2* 0  0  1  0  1  1
-  #~ l3* 1  1  0  0  0  0
-   #~ qui est t = [[1, 1, 0, 1], [0, 1, 0 ,1], [0, 1, 1, 0], [1, 0, 0, 0], [0, 1, 1, 0], [1, 1, 1, 0]]
-  #~ 
-  #~ rendre l'ordre de 
-      #~ c3 c1 c0 c2 c4 c5
-  #~ l3* 0  1  1  0  0  0
-  #~ l0* 1  0  1  0  0  1
-  #~ l2* 0  0  0  1  1  1
-  #~ l1* 0  1  1  1  1  1
-  #~ 
-  #~ la sortie doit donc être : L, C = [3, 0, 2, 1], [3, 1, 0, 2, 4, 5]
-  #~ """
-  #~ 
-  #~ #LE T passé en argument est tel que la première ligne est d'indice 0 (adoption de cet ardre pour toutes les méthodes dès le début)
-  #~ #Mais l'algo est tel que la dernière ligne est d'indice 0
-  #~ #on commence donc par inverser les ligne de T
-  #~ #T = invLigne(T)
-  #~ L, C = [set(range(len(T[0])))], [set(range(len(T)))]
-  #~ pos_l, pos_c = 0, 0
-  #~ pile = [-1]
-  #~ 
-  #~ while pile:
-    #~ c0, c1, l0, l1 = decoupage(pos_l, pos_c, L, C, T)
-    #~ new_pos_c, new_pos_l, L, C = maj(pos_c, pos_l, L, C, c0, c1, l0, l1)
-    #~ #reculer
-    #~ if (new_pos_l == pos_l) and (pos_l + 1 >= len(L)):
-      #~ pos_c -= 1
-      #~ pos_l = pile.pop() + 1
-    #~ else:
-      #~ if (new_pos_c > pos_l):
-        #~ pile.append(pos_l)
-        #~ pos_c += 1
-      #~ pos_l +=  1
-  #~ return L, C
+def algo(T):
+  """
+  à partir d'une table, on rend la liste des lignes et des colonnes après application de l'algo
+  Si on veux, on peut appliquer la méthode nouvelOrdreLC pour réordonner ainsi la table.
+  Exemple :
+  à partir de :
+      c0 c1 c2 c3 c4 c5
+  l0* 1  0  0  1  0  1
+  l1* 1  1  1  0  1  1
+  l2* 0  0  1  0  1  1
+  l3* 1  1  0  0  0  0
+   qui est t = [[1, 1, 0, 1], [0, 1, 0 ,1], [0, 1, 1, 0], [1, 0, 0, 0], [0, 1, 1, 0], [1, 1, 1, 0]]
+  
+  rendre l'ordre de 
+      c3 c1 c0 c2 c4 c5
+  l3* 0  1  1  0  0  0
+  l0* 1  0  1  0  0  1
+  l2* 0  0  0  1  1  1
+  l1* 0  1  1  1  1  1
+  
+  la sortie doit donc être : L, C = [3, 0, 2, 1], [3, 1, 0, 2, 4, 5]
+  """
+  
+  #LE T passé en argument est tel que la première ligne est d'indice 0 (adoption de cet ardre pour toutes les méthodes dès le début)
+  #Mais l'algo est tel que la dernière ligne est d'indice 0
+  #on commence donc par inverser les ligne de T
+  #T = invLigne(T)
+  L, C = [set(range(len(T[0])))], [set(range(len(T)))]
+  pos_l, pos_c = 0, 0
+  pile = [-1]
+  
+  while pile:
+    c0, c1, l0, l1 = decoupage(pos_l, pos_c, L, C, T)
+    new_pos_c, new_pos_l, L, C = maj(pos_c, pos_l, L, C, c0, c1, l0, l1)
+    #reculer
+    if (new_pos_l == pos_l) and (pos_l + 1 >= len(L)):
+      pos_c -= 1
+      pos_l = pile.pop() + 1
+    else:
+      if (new_pos_c > pos_l):
+        pile.append(pos_l)
+        pos_c += 1
+      pos_l +=  1
+  #l'algo considère que le première ligne de la atble est d'indice le plus grand : il faut donc inverser L
+  L.reverse()
+  return L, C
